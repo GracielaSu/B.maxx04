f57f56a07deab4ea835e32e084c01be1979fa40f
Christofeee <117144492+Christofeee@users.noreply.github.com>
2022-11-30 02:29:22 +0630

    nov 30, 2 am save

--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -7,11 +7,20 @@ public class SceneLoader : MonoBehaviour
 {
     public string SceneToLoad;
     public RobotCount robotCount;
+    public GameObject killAllWarning;
+    public GameObject TextBg;
+
+    private float timer = 2;
+    private float time;
+    private bool startTime;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = false;
+        time = timer;
+        killAllWarning.SetActive(false);
+        TextBg.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,11 +33,33 @@ public class SceneLoader : MonoBehaviour
             {
                 LoadScene();
             }
+            else
+            {
+                killAllWarning.SetActive(true);
+                TextBg.SetActive(true);
+                startTime = true;
+            }
         }
     }
 
     void LoadScene()
     {
         SceneManager.LoadScene(SceneToLoad);
+    }
+    
+    void Update()
+    {
+        if(startTime == true)
+        {
+            timer -= 1 * Time.deltaTime;
+        }
+        
+        if(timer <= 0)
+        {
+            killAllWarning.SetActive(false);
+            TextBg.SetActive(false);
+            startTime = false;
+            timer = time;
+        }
     }
 }
