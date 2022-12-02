8c434972505930be677355231c945ed812ba17d0
Christofeee <117144492+Christofeee@users.noreply.github.com>
2022-12-02 21:05:40 +0630

    Player knock back fix initiated

--- Assets/scripts/PlayerKnockB.cs
+++ Assets/scripts/PlayerKnockB.cs
@@ -8,7 +8,14 @@ public class PlayerKnockB : MonoBehaviour
     public float KBForce;
     public float KBCounter;
     public float KBTotalTime;
+
+    public Transform rayCastP;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+
     private float Timer;
+    private RaycastHit2D wallCheck;
+    private RaycastHit2D wallCheck2;
 
     public bool KnockFromRight;
     public bool KB;
@@ -18,11 +25,23 @@ public class PlayerKnockB : MonoBehaviour
     {
         KB = false;
         Timer = KBCounter;
+        wallCheck = Physics2D.Raycast(rayCastP.position, transform.right, rayCastLength, raycastMask);
+        wallCheck2 = Physics2D.Raycast(rayCastP.position, -transform.right, rayCastLength, raycastMask);
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Platform")
+        {
+            target = trig.transform; 
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.DrawRay(rayCastP.position, transform.right * rayCastLength, Color.red);
+        Debug.DrawRay(rayCastP.position, -transform.right * rayCastLength, Color.red);
         if (KB == true)
         {
             if(KnockFromRight == true)
